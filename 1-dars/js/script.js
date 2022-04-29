e3b2bce673afc0139546b8796a1eb5d59fec3614
RuhshonaYusupova <yuspovar@gmail.com>
2022-04-29 08:54:10 -0700

    1-codes

---
+++ 1-dars/js/script.js
@@ -0,0 +1,57 @@
+let n1 = 25,
+    n2 = 2;
+console.log("Number first=" + n1)   
+console.log("Number second=" + n2) 
+
+let n3 = n1 + n2
+let n4 = n1 - n2
+let n5 = n1 / n2
+let n6 = n1 * n2
+let n7 = n1 ** n2
+let n8 = n1 % n2
+
+console.log("Number first + Number second=" + n3)
+console.log("Number first - Number second=" + n4)
+console.log("Number first / Number second=" + n5)
+console.log("Number first * Number second=" + n6)
+console.log("Number first ** Number second=" + n7)
+console.log("Number first % Number second=" + n8)
+// n1= 25
+n1 -= 5
+console.log("Number first - 5= " + n1)
+n1 /= 5
+console.log("Number first / 5= " + n1)
+n1 += 5
+console.log("Number first + 5= " + n1)
+n1 *= 5
+console.log("Number first * 5= " + n1)
+n1 **=5
+console.log("Number first ** 5= " + n1)
+n1 %=5
+console.log("Number first % 5= " + n1)
+
+// n2= 5
+n2 += 5
+console.log("Number second + 5= " + n2)
+n2 *= 5
+console.log("Number second * 5= " + n2)
+n2 -= 5
+console.log("Number second - 5= " + n2)
+n2 /= 5
+console.log("Number second / 5= " + n2)
+n2 **=5
+console.log("Number second ** 5= " + n2)
+n2 %=5
+console.log("Number second % 5= " + n2)
+
+// Alert
+
+let ism = prompt("Ismingizni kiriting")
+let familiya = prompt("Familiyangizni kiriting")
+console.log("Xush kelibsiz " + ism + " " + familiya + "." )
+
+// let yosh = prompt("Tug'ilgan yilingizni yozing")
+// console.log("Sizning yoshingiz " + 2022 - yosh + "da ekan")
+
+
+
