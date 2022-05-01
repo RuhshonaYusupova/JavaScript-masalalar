18b3bac6aabb2e90b4951e87a9ee5cd1d14302c6
RuhshonaYusupova <yuspovar@gmail.com>
2022-05-01 13:41:48 -0700

    3-codes

---
+++ 3-dars/script.js
@@ -0,0 +1,99 @@
+/*
+// Birinchi: A soni musbat
+let a = prompt("Son kiriting")
+a > 0
+? console.log("True") 
+: console.log("False")
+*/
+
+/*
+// Ikkinchi:A soni toq son
+let a = prompt("Son kiriting")
+a % 2 != 0
+?console.log("True")
+:console.log("False")
+*/
+
+/*
+// Uchinchi:A soni juft son
+let a = prompt("Son kiriting")
+a % 2 ==0
+?console.log("True")
+:console.log("False")
+
+*/
+
+/*
+// To'rtinchi: A va B sonlar. A>2 VA B<=3
+let a = prompt("A sonni kiriting")
+let b = prompt("B sonni kiriting")
+a > 2 && b <= 3
+?console.log("True")
+:console.log("False")
+*/
+
+/*
+// Beshinchi: A va B. A>=0 YOKI B< -2
+let a =prompt("A sonni kiriting")
+let b =prompt("B sonni kiriting")
+a >= 0 || b < -2
+?console.log("True")
+:console.log("False")
+*/
+
+/*
+// Oltinchi: A B C. A<=B<=C
+let a =prompt("A sonni kiriting")
+let b =prompt("B sonni kiriting")
+let c =prompt("C sonni kiriting")
+a <= b && b <= c
+?console.log("True")
+:console.log("False")
+*/
+
+/*
+// Sakkizinchi: A va B. A va B sonlar toq sonlar
+let a =prompt("A sonni kiriting")
+let b =prompt("B sonni kiriting")
+a % 2 !=0 && b % 2 !=0
+?console.log("True")
+:console.log("False")
+*/
+
+/*
+// To'qqizinchi:A va B. A va B sonalardan hech bo'lmaganda bittasi toq 
+let a =prompt("A sonni kiriting")
+let b =prompt("B sonni kiriting")
+a % 2 !=0 || b %2 !=0
+?console.log("True")
+:console.log("False")
+*/
+
+
+/*
+// O'n oltinchi:A. Berilga son ikki xonali juft son
+let a = prompt("Ikki xonali son kiriting")
+a % 2 ==0 && a > 9 && a < 100
+?console.log("True")
+:console.log("False")
+*/
+
+
+/*
+// O'n yettinchi:A. Berilga son uch xonali toq son
+let a = prompt("Uch xonali son kiriting")
+a % 2 !=0 && a > 99 && a < 1000
+?console.log("True")
+:console.log("False")
+*/
+
+
+/*
+// O'n sakkizinchi:A B C. Hech  bo'lmaganda 2tasi bir-biriga teng
+let a = prompt("A sonni kiriting")
+let b = prompt("B sonni kiriting")
+let c = prompt("C sonni kiriting")
+a == b || b == c || a == c
+?console.log("True")
+:console.log("False")
+*/
